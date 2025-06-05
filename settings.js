c6d7ebf78d33d2c003e6189ca49efda122534545
Matthew Gromer <matt.gromer@gmail.com>
2025-06-05 11:42:45 -0500

    Add compiled settings module

---
+++ settings.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DEFAULT_SETTINGS = void 0;
+exports.DEFAULT_SETTINGS = {
+    acceptKey: "Tab",
+    noAliasWithShift: false,
+    customDates: {},
+    holidayGroups: {},
+    holidayOverrides: {},
+};
